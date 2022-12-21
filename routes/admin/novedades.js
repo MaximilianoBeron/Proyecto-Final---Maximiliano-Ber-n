ba39f2406d50d5cff83915e4736b514e7804485b
Maximiliano Berón <mberon@hcdn.gob.ar>
2022-12-21 02:15:08 -0300

    Realicé actualizaciones al proyecto para completar la tarea del M5U3

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,12 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
-var util = require('util')
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 
 // para listar las novedades
 router.get('/', async function (req, res, next) {
+
     var novedades = await novedadesModel.getNovedades();
+
+    novedades = novedades.map(novedad => {
+        if (novedad.img_id) {
+
+            const imagen = cloudinary.image(novedad.img_id, {
+                width: 50,
+                height: 50,
+                crop: 'fill'
+            });
+
+            return {
+                ...novedad,
+                imagen
+            }
+
+        } else {
+            return {
+                ...novedad,
+                imagen: ''
+            }
+        }
+    });
 
     res.render('admin/novedades', {
         layout: 'admin/layout',
@@ -19,6 +45,11 @@ router.get('/', async function (req, res, next) {
 // INICIO para Eliminar por id
 router.get('/eliminar/:id', async (req, res, next) => {
     const id = req.params.id;
+    let novedad = await novedadesModel.getNovedadesbyId(id);
+    if (novedad.img_id) {
+        await (destroy(novedad.img_id));
+    }
+
 
     await novedadesModel.deleteNovedadesbyId(id);
     res.redirect('/admin/novedades')
@@ -38,8 +69,21 @@ router.get('/agregar', (req, res, next) => {
 // INICIO para Insertar
 router.post('/agregar', async (req, res, next) => {
     try {
+
+        var img_id = '';
+        if (req.files && Object.keys(req.files).length > 0) {
+            imagen = req.files.imagen;
+            img_id = (await uploader(imagen.tempFilePath)).public_id;
+        }
+
+
+
+
         if (req.body.lugar != "" && req.body.fecha != "" && req.body.ubicacion != "") {
-            await novedadesModel.insertNovedades(req.body);
+            await novedadesModel.insertNovedades({
+                ...req.body,  //spread
+                img_id
+            });
             res.redirect('/admin/novedades')
 
         } else {
@@ -75,12 +119,36 @@ router.get('/modificar/:id', async (req, res, next) => {
 
 router.post('/modificar', async (req, res, next) => {
     try {
-        console.log(req.body.id);
+        let img_id = req.body.img_original;
+        let borrar_img_vieja = false;
+
+        if (req.body.img_delete === "1") {
+            img_id = null;
+            borrar_img_vieja = true;
+        } else {
+            if (req.files && Object.keys(req.files).length > 0) {
+                imagen = req.files.imagen;
+                img_id = (await uploader(imagen.tempFilePath)).public_id;
+                borrar_img_vieja = true;
+            }
+        }
+        if (borrar_img_vieja && req.body.img_original) {
+            await (destroy(req.body.img_original));
+        }
+
+
+
+
         var obj = {
             lugar: req.body.lugar,
             fecha: req.body.fecha,
-            ubicacion: req.body.ubicacion
+            ubicacion: req.body.ubicacion,
+            img_id
         }
+
+
+
+
 
 
         console.log(obj)
